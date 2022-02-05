80b8b15e152249a001d5ec7d6cae6bd57bac5997
ndhaka007 <nikhil.dhaka@razorpay.com>
2022-02-05 17:36:24 +0530

    first commit

---
+++ src/pages/connectWallet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './connectWallet.css';
+  
+const connectWallet = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Welcome to ConnectWallet</h1>
+    </div>
+  );
+};
+  
+export default connectWallet;
